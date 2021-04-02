4deec7d06770954488e734b45f85a057302bd6c8
Massipssa <kerrache.massipssa@gmail.com>
2021-04-02 19:45:03 +0200

    Update docker_ci.sh

--- scripts/docker_ci.sh
+++ scripts/docker_ci.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USER="massipssa"
-REPO="devs"
+REPO="test-repo"
 TAG="datasource"
 docker build -t "$USER"/"$REPO":"$TAG" .
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
